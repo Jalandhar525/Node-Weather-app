949cf2430e392e1f62d6763c6b354432e37f5929
Ganga Jalandhar Reddy <jalandhar27m@gmail.com>
2021-06-02 11:30:39 +0530

    Setup for Heroku

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const hbs = require('hbs');
 const request = require('request');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 const publicDirectoryPath = path.join(__dirname, '../public');
 const viewsPath = path.join(__dirname, '../templates');
@@ -78,6 +79,6 @@ app.get('*', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('Server Up and Running');
 });
